117457eae206725a575da4cfbfa7647e78884ec7
Marcos Sánchez <musanchez@uamv.edu.ni>
2023-04-30 13:00:00 -0600

    Error previo corregido, agregadas librerías para trabajar Swagger :clown_face:

---
+++ SISPAC/src/main/java/com/uam/SISPAC/SispacApplication.java
@@ -0,0 +1,13 @@
+package com.uam.SISPAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispacApplication.class, args);
+	}
+
+}
---
+++ SISPAC/src/test/java/com/uam/SISPAC/SispacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.SISPAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SispacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
