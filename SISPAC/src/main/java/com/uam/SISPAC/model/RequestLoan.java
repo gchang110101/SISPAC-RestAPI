6eb9a434d06195aaf1d3d8f7f51d98722f5e36f5
Marcos Sánchez <musanchez@uamv.edu.ni>
2023-05-01 12:40:43 -0600

    Cambios menores

--- SISPAC/src/main/java/com/uam/SISPAC/model/RequestLoan.java
+++ SISPAC/src/main/java/com/uam/SISPAC/model/RequestLoan.java
@@ -1,11 +1,13 @@
 package com.uam.SISPAC.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 
+@Entity(name = "RequestLoan")
 public class RequestLoan {
     @Id
     private String id;
