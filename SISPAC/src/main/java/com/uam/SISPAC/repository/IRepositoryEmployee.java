0b0dd3512a8b7644769a7366a2789a908bdf8ccf
Gabriel Chang <gdchang@uamv.edu.ni>
2023-05-15 11:01:07 -0600

    deleted some repos and added ec-ac's branch changes manually

--- SISPAC/src/main/java/com/uam/SISPAC/repository/IRepositoryEmployee.java
+++
@@ -1,8 +0,0 @@
-package com.uam.SISPAC.repository;
-
-import com.uam.SISPAC.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositoryEmployee extends JpaRepository<Employee, String> {
-
-}
--- SISPAC/src/main/java/com/uam/SISPAC/repository/IRepositoryTeacher.java
+++
@@ -1,8 +0,0 @@
-package com.uam.SISPAC.repository;
-
-import com.uam.SISPAC.model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositoryTeacher extends JpaRepository<Teacher, String> {
-
-}
