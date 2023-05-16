2fbc7398aa91c87c462bcbf42b3605b47472fe2f
Gabriel Chang <gdchang@uamv.edu.ni>
2023-05-15 23:46:50 -0600

    added login response

--- SISPAC/src/main/java/com/uam/SISPAC/repository/humanresources/IRepositorySystemUser.java
+++ SISPAC/src/main/java/com/uam/SISPAC/repository/humanresources/IRepositorySystemUser.java
@@ -2,9 +2,12 @@ package com.uam.SISPAC.repository.humanresources;
 
 import com.uam.SISPAC.model.humanresources.SystemUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRepositorySystemUser extends JpaRepository<SystemUser, String> {
-
+    @Query("select e from SystemUser e where e.names = :user and e.password = :password")
+    public SystemUser getSystemUserByUser(@Param("user") String user, @Param("password")  String password);
 }
--- SISPAC/src/main/java/com/uam/SISPAC/service/humanresources/ServiceSystemUser.java
+++ SISPAC/src/main/java/com/uam/SISPAC/service/humanresources/ServiceSystemUser.java
@@ -1,5 +1,6 @@
 package com.uam.SISPAC.service.humanresources;
 
+import com.uam.SISPAC.dto.humanresources.LoginResponse;
 import com.uam.SISPAC.model.humanresources.SystemUser;
 import com.uam.SISPAC.repository.humanresources.IRepositorySystemUser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +20,25 @@ public class ServiceSystemUser implements IServiceSystemUser {
     public List<SystemUser> getAll() {
         return repo.findAll();
     }
-
     @Override
     public void create(SystemUser systemUser) {
         repo.save(systemUser);
     }
-
     @Override
     public Optional<SystemUser> getOne(String id) {
         return repo.findById(id);
+    }
+
+    @Override
+    public LoginResponse getUser(String names, String password) {
+        SystemUser u = repo.getSystemUserByUser(names, password);
+        LoginResponse lr = new LoginResponse(false, "NOT CONNECTED");
+
+        if(u != null) {
+            lr.setSuccess(true);
+            lr.setMsg("USER CONNECTED!");
+        }
+
+        return lr;
     }
 }
